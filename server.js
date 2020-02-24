be97dce97850d36981b3f7b1de3a70b077d14167
Janet <Reid>
2020-02-24 21:50:34 +0800

    Set up layout and authentication folders and files

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
-// app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/booking', require('./routes/api/booking'));
 app.use('/api/pet', require('./routes/api/pet'));
-// app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
