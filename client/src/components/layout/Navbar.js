e3c979e91ac5279d1ef63f462b8b6defe9c304ac
Janet <Reid>
2020-02-28 22:07:31 +0800

    initial setup for profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,9 +4,15 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-export const Navbar = ({ auth: { isAuthenticated, loading} , logout }) => {
+export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
       <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,29 @@ const _ = require('lodash');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route POST api/profile
+// @desc Create or update user profile
+// 
+
 router.post('/', auth, async (req, res) => {
   try {
     let newProfile = new Profile(req.body);
