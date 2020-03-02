222c79c8e9b864fe9de1502c6c6d2442ef13bcaf
Janet <Reid>
2020-03-02 22:34:59 +0800

    Created new dashboard items

---
+++ client/src/components/dashboard/Bookings.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { deleteBooking } from '../../actions/booking';
+
+const Bookings = ({ bookinginfo, deleteBooking }) => {
+  const bookings = bookinginfo.map(book => (
+    <tr key={book._id}>
+      <td>
+        <Moment format='YYYY/MM/DD'>{book.from}</Moment>
+      </td>
+      <td>
+        <Moment format='YYYY/MM/DD'>{book.to}</Moment>
+      </td>
+      <td>
+        <button
+          onClick={() => deleteBooking(book._id)}
+          className='btn btn-danger'
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  ));
+  return (
+    <Fragment>
+      <br />
+      <h2 className='my-2'>Booking Information</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>From</th>
+            <th>To</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{bookings}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Bookings.propTypes = {
+  bookinginfo: PropTypes.array.isRequired,
+  deleteBooking: PropTypes.func.isRequired
+};
+
+export default connect(null, { deleteBooking })(Bookings);
---
+++ client/src/components/dashboard/Pets.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deletePet } from '../../actions/profile';
+
+const Pets = ({ petinfo, deletePet }) => {
+  const pets = petinfo.map(pet => (
+    <tr key={pet._id}>
+      <td>{pet.pet}</td>
+      <td className='hide-sm'>{pet.animal}</td>
+      <td className='hide-sm'>{pet.breed}</td>
+      <td>
+        <button onClick={() => deletePet(pet._id)} className='btn btn-danger'>
+          Delete
+        </button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <Fragment>
+      <br />
+      <h2 className='my-2'>Pet Information</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Pet</th>
+            <th className='hide-sm'>Animal</th>
+            <th className='hide-sm'>Breed</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{pets}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Pets.propTypes = {
+  petinfo: PropTypes.array.isRequired,
+  deletePet: PropTypes.func.isRequired
+};
+
+export default connect(null, { deletePet })(Pets);
