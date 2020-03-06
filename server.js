2e0fcdbdad6bb0783787419a53f3b200a00cab5d
Janet <Reid>
2020-03-06 23:18:22 +0800

    Server routes finished

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.get('/', (req, res) => res.send('API Running'));
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/booking', require('./routes/api/booking'));
-//app.use('/api/pet', require('./routes/api/pet'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
