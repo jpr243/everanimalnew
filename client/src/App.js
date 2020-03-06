ab02ad66e8c10304e8b55b5b1ee8c658071e20a1
Janet <Reid>
2020-03-06 23:37:18 +0800

    Cleaned up code in App.js and new Route file

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import CreateBooking from './components/profile-forms/CreateBooking';
-import AddPet from './components/profile-forms/AddPet';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -20,44 +12,22 @@ import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
 const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser());
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+      store.dispatch(loadUser());
+    }
   }, []);
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/create-booking'
-                component={CreateBooking}
-              />
-              <PrivateRoute exact path='/add-pet' component={AddPet} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profile-forms/CreateProfile';
+import EditProfile from '../profile-forms/EditProfile';
+import AddPet from '../profile-forms/AddPet';
+import CreateBooking from '../profile-forms/CreateBooking';
+import PrivateRoute from '../routing/PrivateRoute';
+
+const Routes = () => {
+  return (
+    <section className='container'>
+      <Alert />
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+        <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+        <PrivateRoute exact path='/add-pet' component={AddPet} />
+        <PrivateRoute exact path='/add-booking' component={AddBooking} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes;
